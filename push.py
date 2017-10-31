2958bdaec980678160c558c0ceea4222c759b63a
Ali Shahbour <shahbour@gmail.com>
2017-10-31 23:11:46 +0200

    update to save data

--- push.py
+++ push.py
@@ -1,11 +1,15 @@
 import json
 import logging
+import boto3
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def saveData(event, context):
+def save_data(event, context):
 
     logger.info('got event{}'.format(event))
 
@@ -19,7 +23,6 @@ def saveData(event, context):
     # Retrieve the phone number from path
     phoneNumber = event['pathParameters']['phoneNumber']
 
-
     body = {
         "phoneNumber": phoneNumber,
         "token": data['token']
@@ -27,11 +30,21 @@ def saveData(event, context):
 
     # Check this example on how to save dynamodb https://github.com/serverless/examples/tree/master/aws-python-rest-api-with-dynamodb
 
+    table = dynamodb.Table("rushup-mobilehub-1263655713-user-push")
+
+    item = {
+        'userId': str(uuid.uuid1()),
+        'phone': int(phoneNumber),
+        'token': data['token']
+    }
+
+    table.put_item(Item=item)
+
     logger.info('got body{}'.format(body))
 
     response = {
         "statusCode": 201,
-        "body": json.dumps(body)
+        "body": json.dumps(item)
     }
 
     return response
