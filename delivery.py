f556fd0164b16c401021f112b75555524c987c95
Ali Shahbour <shahbour@gmail.com>
2017-12-28 18:03:31 +0200

    save delivery and send push on saving

--- delivery.py
+++ delivery.py
@@ -1,6 +1,8 @@
 import boto3
 import json
 import uuid
+import time
+import push
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
 
@@ -14,6 +16,8 @@ def save_delivery(event, context):
 
     if "id"  not in delivery:
         delivery["id"] = str(uuid.uuid1())
+        delivery["status"] = "pending"
+        delivery["delivery_date"] = int(time.time())
 
     table.put_item(
         Item=delivery
@@ -22,7 +26,65 @@ def save_delivery(event, context):
     # print(delivery.from)
     print(delivery)
 
+    push_request = dict()
+    push_request["message"] = delivery
+    push_request["type"] = "delivery_request"
+    push_request["phone"] = delivery["to"]
 
-class Delivery:
-    def __init__(self, json):
-        vars(self).update(json)
+    body = dict()
+    body["body"] = json.dumps(push_request)
+
+    push.publish_message(body, context)
+
+    response = {
+        "statusCode": 201,
+        "body": json.dumps(delivery)
+    }
+
+    return response
+
+
+def update_delivery_status(event, context):
+
+    print json.dumps(event,  encoding='ascii')
+
+    delivery_id = event["pathParameters"]["delivery_id"]
+
+    body = json.loads(event['body'])
+
+    status = body["status"]
+
+    key = {
+        'id': delivery_id
+    }
+
+    table.update_item(
+        Key=key,
+        UpdateExpression='SET status = :val1',
+        ExpressionAttributeValues={
+            'val1': status
+        }
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({})
+    }
+
+    return response
+
+
+def retrieveDelivery(delivery_id):
+
+    key = {
+        'id': delivery_id
+    }
+
+    result = table.get_item(Key=key)
+
+    print json.dumps(result, encoding='ascii')
+
+    if "Item" in result:
+        return result["Item"]
+    else:
+        return None
