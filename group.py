676e40d0a4913e3c478bd652ac73aca5b6852090
nancy <n.boukamel@zeroandone.me>
2018-02-13 15:36:47 +0200

    Add Group and Assign User to a Group

---
+++ group.py
@@ -0,0 +1,47 @@
+import boto3
+import json
+import os
+
+cognito = boto3.client('cognito-idp')
+
+def save(event,context):
+
+    print json.dumps(event,  encoding='ascii')
+    body = json.loads(event['body'])
+    group_name =body['group_name']
+    userPoolId=os.environ["identityPoolId"]
+
+    group = cognito.create_group(
+    GroupName=group_name,
+    UserPoolId=userPoolId,
+    Description="Company group"
+    )
+    print(group)
+    response = {
+    "statusCode": 201,
+    "body": "{}"
+   }
+    return response
+
+
+def assign_user(event,context):
+
+    print json.dumps(event,  encoding='ascii')
+    body= json.loads(event['body'])
+    email=body['email']
+    group_name=body['group_name']
+
+    print ("group name "+group_name);
+
+    addUser = cognito.admin_add_user_to_group(
+        UserPoolId=os.environ["identityPoolId"],
+        Username=email,
+        GroupName=group_name
+    )
+
+    response = {
+        "statusCode": 201,
+        "body": "{}"
+    }
+    return response
+
